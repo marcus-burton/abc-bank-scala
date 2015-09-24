dc136a402e5e878225dbd586927a506d663f44b8
DARKZ <DARKZ@DARKZ-PC.home>
2015-09-24 15:17:43 -0400

    Refactor

--- src/main/scala/com/abc/Account.scala
+++ src/main/scala/com/abc/Account.scala
@@ -1,44 +1,59 @@
 package com.abc
 
-import scala.collection.mutable.ListBuffer
+import java.util.ArrayList
+import java.util.Collections
 
-object Account {
-  final val CHECKING: Int = 0
-  final val SAVINGS: Int = 1
-  final val MAXI_SAVINGS: Int = 2
-}
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.JavaConversions.asScalaIterator
 
-class Account(val accountType: Int, var transactions: ListBuffer[Transaction] = ListBuffer()) {
+sealed trait Account {
+  val transactions = Collections.synchronizedList(new ArrayList[Transaction]())
 
-  def deposit(amount: Double) {
+  def deposit(amount: Double): Unit = {
     if (amount <= 0)
       throw new IllegalArgumentException("amount must be greater than zero")
     else
       transactions += Transaction(amount)
   }
 
-  def withdraw(amount: Double) {
+  def withdraw(amount: Double): Unit = {
     if (amount <= 0)
       throw new IllegalArgumentException("amount must be greater than zero")
     else
       transactions += Transaction(-amount)
   }
 
-  def interestEarned: Double = {
-    val amount: Double = sumTransactions()
-    accountType match {
-      case Account.SAVINGS =>
-        if (amount <= 1000) amount * 0.001
-        else 1 + (amount - 1000) * 0.002
-      case Account.MAXI_SAVINGS =>
-        if (amount <= 1000) return amount * 0.02
-        if (amount <= 2000) return 20 + (amount - 1000) * 0.05
-        70 + (amount - 2000) * 0.1
-      case _ =>
-        amount * 0.001
-    }
+  def interestEarned: Double
+
+  def sumTransactions(checkAllTransactions: Boolean = true): Double = transactions.synchronized {
+    transactions.iterator.map(_.amount).sum
   }
 
-  def sumTransactions(checkAllTransactions: Boolean = true): Double = transactions.map(_.amount).sum
+  def transactionSummary(f: Transaction => String,
+                         begin: String,
+                         sep: String,
+                         end: String) = transactions.synchronized {
+    transactions.iterator.map(f).mkString(begin, sep, end)
+  }
+}
 
+case class CheckingAccount() extends Account {
+  def interestEarned: Double = sumTransactions() * 0.001
+}
+
+case class SavingsAccount() extends Account {
+  def interestEarned: Double = {
+    val amount = sumTransactions()
+    if (amount <= 1000) amount * 0.001
+    else 1 + (amount - 1000) * 0.002
+  }
+}
+
+case class MaxiSavingsAccount() extends Account {
+  def interestEarned: Double = {
+    val amount = sumTransactions()
+    if (amount <= 1000) return amount * 0.02
+    if (amount <= 2000) return 20 + (amount - 1000) * 0.05
+    70 + (amount - 2000) * 0.1
+  }
 }
