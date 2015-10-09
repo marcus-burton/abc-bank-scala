99debe90db051fdf737ad2b0e2a2bd7f347c48a7
Hugh Richardson <hugh.richardson@patch.com>
2015-10-08 22:39:24 -0400

    remove usage of new

--- src/test/scala/com/abc/BankTest.scala
+++ src/test/scala/com/abc/BankTest.scala
@@ -6,15 +6,15 @@ class BankTest extends FlatSpec with Matchers {
 
   "Bank" should "customer summary" in {
     val bank: Bank = new Bank
-    var john: Customer = new Customer("John").openAccount(new Account(CHECKING))
+    var john: Customer = Customer("John").openAccount(Account(CHECKING))
     bank.addCustomer(john)
     bank.customerSummary should be("Customer Summary\n - John (1 account)")
   }
 
   it should "checking account" in {
     val bank: Bank = new Bank
-    val checkingAccount: Account = new Account(CHECKING)
-    val bill: Customer = new Customer("Bill").openAccount(checkingAccount)
+    val checkingAccount: Account = Account(CHECKING)
+    val bill: Customer = Customer("Bill").openAccount(checkingAccount)
     bank.addCustomer(bill)
     checkingAccount.deposit(100.0)
     bank.totalInterestPaid should be(0.1)
