fc5367881e773a06d3693d6369521cb36b0bf71e
Hugh Richardson <hugh.richardson@patch.com>
2015-10-09 00:05:06 -0400

    test description

--- src/test/scala/com/abc/TransactionTest.scala
+++ src/test/scala/com/abc/TransactionTest.scala
@@ -3,7 +3,7 @@ package com.abc
 import org.scalatest.{FlatSpec, Matchers}
 
 class TransactionTest extends FlatSpec with Matchers {
-  "Transaction" should "type" in {
+  "Transaction" should "set correct amount" in {
     val t = Transaction(5)
     t.amount should be(5)
   }
