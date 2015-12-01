b48492d2389af8404fdb964d723b6000a6a76d63
Vincent Tam <vincent.tam@outlook.com>
2015-11-30 23:33:10 -0500

    first commit

--- src/main/scala/com/abc/Bank.scala
+++ src/main/scala/com/abc/Bank.scala
@@ -23,22 +23,15 @@ class Bank {
   def totalInterestPaid: Double = {
     var total: Double = 0
     for (c <- customers) total += c.totalInterestEarned
-    return total
+    total
   }
 
   def getFirstCustomer: String = {
-    try {
-      customers = null
-      customers(0).name
-    }
-    catch {
-      case e: Exception => {
-        e.printStackTrace
-        return "Error"
-      }
-    }
+    if(customers.nonEmpty )
+      customers.head.name
+    else
+      "Error"
   }
-
 }
 
 
