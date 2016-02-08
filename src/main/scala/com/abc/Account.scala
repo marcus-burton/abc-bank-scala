84b79ef15e0bcc2944875d4531aeeff9ff7d3d87
yuri@bakumenko.com <bofa-test-yuri@bakumenko.com>
2016-02-08 08:15:11 -0500

    increased test coverage for interest earnings

--- src/main/scala/com/abc/Account.scala
+++ src/main/scala/com/abc/Account.scala
@@ -6,39 +6,47 @@ object Account {
   final val CHECKING: Int = 0
   final val SAVINGS: Int = 1
   final val MAXI_SAVINGS: Int = 2
+
+  final val BPS:BigDecimal = BigDecimal(0.0001)
 }
 
-class Account(val accountType: Int, val transactions: ListBuffer[Transaction] = ListBuffer()) {
+case class InsufficientFundsException(msg:String) extends RuntimeException
+case class InvalidAmountException(msg:String) extends RuntimeException
 
-  def deposit(amount: BigDecimal) {
-    if (amount <= 0)
-      throw new IllegalArgumentException("amount must be greater than zero")
-    else
-      transactions += Transaction(amount)
+case class Account(val accountType: Int, private val transactions: ListBuffer[Transaction] = ListBuffer()) {
+  def checkAmount(amount:BigDecimal):Boolean = {
+    amount > 0 && (amount *100).isWhole()
   }
 
-  def withdraw(amount: BigDecimal) {
-    if (amount <= 0)
-      throw new IllegalArgumentException("amount must be greater than zero")
-    else
-      transactions += Transaction(-amount)
+  def deposit(amount: BigDecimal) = checkAmount(amount) match {
+    case false => throw new InvalidAmountException("Can't deposit as amount must be greater than zero and can't have fractions of the cents")
+    case true => transactions += Transaction(amount)
   }
+
+  def withdraw(amount: BigDecimal) = checkAmount(amount) match {
+    case false => throw new InvalidAmountException("Can't withdraw as amount must be greater than zero and can't have fractions of the cents")
+    case true => (this.sumTransactions >= amount) match {
+        case true => transactions += Transaction(-amount)
+        case false => throw new InsufficientFundsException("Can't withdraw more funds than it is available")}
+  }
+
+  def transactionAmounts:List[BigDecimal] = transactions.map(_.amount).toList
 
   def interestEarned: BigDecimal = {
-    val amount: BigDecimal = sumTransactions()
+    val amount: BigDecimal = sumTransactions
     accountType match {
       case Account.SAVINGS =>
         if (amount <= 1000) amount * 0.001
         else 1 + (amount - 1000) * 0.002
       case Account.MAXI_SAVINGS =>
         if (amount <= 1000) return amount * 0.02
-        if (amount <= 2000) return 20 + (amount - 1000) * 0.05
-        70 + (amount - 2000) * 0.1
+        else if (amount <= 2000) return 20 + (amount - 1000) * 0.05
+        else 70 + (amount - 2000) * 0.1
       case _ =>
         amount * 0.001
     }
   }
 
-  def sumTransactions(checkAllTransactions: Boolean = true): BigDecimal = transactions.map(_.amount).sum
+  def sumTransactions: BigDecimal = transactions.map(_.amount).sum
 
 }
--- src/main/scala/com/abc/Customer.scala
+++ src/main/scala/com/abc/Customer.scala
@@ -2,6 +2,8 @@ package com.abc
 
 import scala.collection.mutable.ListBuffer
 
+case class TransferFundsException(msg:String) extends RuntimeException
+
 class Customer(val name: String, val accounts: ListBuffer[Account] = ListBuffer()) {
 
   def openAccount(account: Account): Customer = {
@@ -13,11 +15,18 @@ class Customer(val name: String, val accounts: ListBuffer[Account] = ListBuffer(
 
   def totalInterestEarned: BigDecimal = accounts.map(_.interestEarned).sum
 
+  def transferFunds(from: Account, to:Account, amount: BigDecimal):Unit = {
+    if (from == to) throw new TransferFundsException("Can't transact within the same account")
+    if (!(accounts contains from) || !(accounts contains to)) throw new TransferFundsException("Both accounts should be owned by the same customer")
+    from.withdraw(amount)
+    to.deposit(amount)
+  }
+
   /**
    * This method gets a statement
    */
   def getStatement: String = {
-    val totalAcrossAllAccounts = accounts.map(_.sumTransactions()).sum
+    val totalAcrossAllAccounts = accounts.map(_.sumTransactions).sum
     val statement = f"Statement for $name\n" +
       accounts.sortWith(_.accountType < _.accountType).map(statementForAccount).mkString("\n", "\n\n", "\n") +
       s"\nTotal In All Accounts ${toDollars(totalAcrossAllAccounts)}"
@@ -33,14 +42,14 @@ class Customer(val name: String, val accounts: ListBuffer[Account] = ListBuffer(
       case Account.MAXI_SAVINGS =>
         "Maxi Savings Account\n"
     }
-    val transactionSummary = a.transactions.map(t => withdrawalOrDepositText(t) + " " + toDollars(t.amount.abs))
+    val transactionSummary = a.transactionAmounts.map(a => withdrawalOrDepositText(a) + " " + toDollars(a.abs))
       .mkString("  ", "\n  ", "\n")
-    val totalSummary = s"Total ${toDollars(a.transactions.map(_.amount).sum)}"
+    val totalSummary = s"Total ${toDollars(a.sumTransactions)}"
     accountType + transactionSummary + totalSummary
   }
 
-  private def withdrawalOrDepositText(t: Transaction) =
-    t.amount match {
+  private def withdrawalOrDepositText(a: BigDecimal) =
+    a match {
       case a if a < 0 => "withdrawal"
       case a if a > 0 => "deposit"
       case _ => "N/A"
