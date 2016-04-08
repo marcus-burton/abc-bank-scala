a32559ea79f7e2c5cd4c636b133d6222bc9284b3
Pedro Rodriguez <pedro@pjrt.co>
2016-04-08 18:03:16 +0000

    No-op
    Remove random `null` statement variable and moved the "accounts"
    variable into the class body.

--- src/main/scala/com/abc/Customer.scala
+++ src/main/scala/com/abc/Customer.scala
@@ -2,10 +2,12 @@ package com.abc
 
 import scala.collection.mutable.ListBuffer
 
-class Customer(val name: String, val accounts: ListBuffer[Account] = ListBuffer()) {
+class Customer(val name: String) {
+
+  val accounts: ListBuffer[Account] = ListBuffer.empty
 
   def openAccount(account: Account): Customer = {
-    accounts = accounts :+ account
+    accounts += account
     this
   }
 
@@ -17,14 +19,10 @@ class Customer(val name: String, val accounts: ListBuffer[Account] = ListBuffer(
    * This method gets a statement
    */
   def getStatement: String = {
-    //JIRA-123 Change by Joe Bloggs 29/7/1988 start
-    var statement: String = null //reset statement to null here
-    //JIRA-123 Change by Joe Bloggs 29/7/1988 end
     val totalAcrossAllAccounts = accounts.map(_.sumTransactions()).sum
-    statement = f"Statement for $name\n" +
+    f"Statement for $name\n" +
       accounts.map(statementForAccount).mkString("\n", "\n\n", "\n") +
       s"\nTotal In All Accounts ${toDollars(totalAcrossAllAccounts)}"
-    statement
   }
 
   private def statementForAccount(a: Account): String = {
@@ -36,8 +34,10 @@ class Customer(val name: String, val accounts: ListBuffer[Account] = ListBuffer(
       case MaxiSavingsAccount() =>
         "Maxi Savings Account\n"
     }
-    val transactionSummary = a.transactions.map(t => withdrawalOrDepositText(t) + " " + toDollars(t.amount.abs))
-      .mkString("  ", "\n  ", "\n")
+    val transactionSummary =
+      a.transactions
+        .map(t => withdrawalOrDepositText(t) + " " + toDollars(t.amount.abs))
+        .mkString("  ", "\n  ", "\n")
     val totalSummary = s"Total ${toDollars(a.transactions.map(_.amount).sum)}"
     accountType + transactionSummary + totalSummary
   }
