5cd9525c5027462e1827172c000e1f9bc65b93c6
Henry Katz <henry.katz@iscs-i.com>
2016-04-18 02:09:34 -0400

    Add xfr functionality and use val in getStatement

--- src/main/scala/com/abc/Customer.scala
+++ src/main/scala/com/abc/Customer.scala
@@ -18,10 +18,10 @@ class Customer(val name: String, var accounts: ListBuffer[Account] = ListBuffer(
    */
   def getStatement: String = {
     //JIRA-123 Change by Joe Bloggs 29/7/1988 start
-    var statement: String = null //reset statement to null here
+    //var statement: String = null //reset statement to null here
     //JIRA-123 Change by Joe Bloggs 29/7/1988 end
     val totalAcrossAllAccounts = accounts.map(_.sumTransactions()).sum
-    statement = f"Statement for $name\n" +
+    val statement = f"Statement for $name\n" +
       accounts.map(statementForAccount).mkString("\n", "\n\n", "\n") +
       s"\nTotal In All Accounts ${toDollars(totalAcrossAllAccounts)}"
     statement
@@ -50,5 +50,20 @@ class Customer(val name: String, var accounts: ListBuffer[Account] = ListBuffer(
     }
 
   private def toDollars(number: Double): String = f"$$$number%.2f"
+
+  /**
+    * simple transfer assume no restriction other than a contains funds
+    * @param a from acct
+    * @param b to acct
+    * @param amt how much to move
+    */
+  def transfer(a: Account, b: Account, amt: Double): Boolean = {
+    if (a.sumTransactions() >= amt) {
+      a.withdraw(amt)
+      b.deposit(amt)
+      true
+    } else
+      false
+  }
 }
 
