d92d0187b5a0ae8c8f3a964ae913e1d35480df24
Serge Shuster <serge_shuster@yahoo.com>
2016-05-09 23:44:47 -0400

    Implemented all features

---
+++ src/main/scala/com/abc/bankrun.scala
@@ -0,0 +1,30 @@
+package com.abc
+
+import Account._
+
+object main extends App  {
+  val account1 = Savings("John")
+  val bank = Bank("bank1")
+  bank.addAccount(account1,5)
+    .deposit(account1, 100, 11)
+    .deposit(account1, 1000, 15)
+    .deposit(account1, 2000, 18)
+    .deposit(account1, 5000, 20)
+    .customerReport(account1,40)
+
+  val account2 = Checking("Jim")
+  bank.addAccount(account2,5)
+    .deposit(account2, 100, 11)
+    .deposit(account2, 1000, 15)
+    .deposit(account2, 2000, 18)
+    .deposit(account2, 5000, 20)
+    .customerReport(account2,40)
+
+  val account3 = MaxSavings("Jack")
+  bank.addAccount(account3,5)
+    .deposit(account3, 100, 11)
+    .deposit(account3, 1000, 15)
+    .deposit(account3, 2000, 18)
+    .deposit(account3, 5000, 20)
+    .customerReport(account3,40)
+}
