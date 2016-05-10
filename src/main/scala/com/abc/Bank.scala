3ce8305d4189dd153eda29bc03d4e627bfc585ce
Serge Shuster <serge_shuster@yahoo.com>
2016-05-09 23:50:17 -0400

    Implemented all features

--- src/main/scala/com/abc/Bank.scala
+++ src/main/scala/com/abc/Bank.scala
@@ -5,36 +5,50 @@ import Account._
 case class Bank(name: String, transactions: List[Transaction] = List()) {
  
    def doTransfer(accFrom: Account, accTo: Account, amount: Double, time: Int): Bank =
-     this.copy(transactions = 
-        Transaction(accFrom,-amount,time)::
-        Transaction(accTo,amount,time)::
-        transactions)
+     this.copy(transactions = transactions :+
+        Transaction(accFrom,-amount,time) :+
+        Transaction(accTo,amount,time))
 
    def deposit(acc: Account, amount: Double, time: Int): Bank =
-        this.copy(transactions = 
-        Transaction(acc,amount,time)::
-        transactions)
+        this.copy(transactions =
+          transactions :+ Transaction(acc,amount,time))
   
-   def addAccount(account: Account, time: Int): Bank = {
-      this.copy(transactions = Transaction(account,0,time)::transactions)
+   def addAccount(acc: Account, time: Int): Bank = {
+      this.copy(transactions = transactions :+ Transaction(acc,0,time))
    }
-    
-  def customerReport: String = {
-    var out=""
+  
+  def customerReport(acc: Account, ctime: Int) : String = {
+    var out = ""
     val rep = transactions.groupBy(_.account.owner).map {
-      case (key, value) => (key, value.map(_.amount).sum)
-     }.foreach {
-           case (name, total) =>
-           println(s"$name, $total")
-           out += s"$name, $total"
-      }
-  out
+      case (key, value) =>
+        var total = 0.0
+        val i1 = value.toIterator
+        var i = 0
+        var j = 0 // numbers of days since last withdrawal
+        var l1 = i1.next()
+        while (i < ctime) {
+          if (i == l1.time) {
+            total += l1.amount
+            if (l1.amount < 0) j = 0
+            if (i1.hasNext) l1 = i1.next()
+          }
+          total += interest(acc,total,j)
+          i += 1
+          j += 1
+        }
+        (key,total)
+    }.foreach {
+      case (name, total) =>
+        println(s"$name, $total")
+        out += s"$name, $total"
+    }
+    out
   }
  
- def interest(acctType: Account, amount: Double): Double = acctType match {
+ def interest(acctType: Account, amount: Double, lw: Int): Double = acctType match {
     case _: Checking => checkingInterest(amount)
     case _: Savings => savingsInterest(amount)
-    case _: MaxSavings => maxSavingsInterest(amount)
+    case _: MaxSavings => maxSavingsInterest(amount, lw)
   }
   
   def checkingInterest(amount: Double) = {
@@ -50,14 +64,15 @@ case class Bank(name: String, transactions: List[Transaction] = List()) {
       savingsInterest(1000) + (amount - 1000) * 0.002
   }
   
-  def maxSavingsInterest(amount: Double): Double = {
+  def maxSavingsInterest(amount: Double, lw: Int): Double = {
+    val intr = if (lw > 10) 0.1 else 0.05
     require(amount >= 0) 
     if (amount <= 1000) 
       amount * 0.02
     else if (amount > 1000 && amount <= 2000) 
-      maxSavingsInterest(1000) + (amount - 1000) * 0.05
+      maxSavingsInterest(1000, lw) + (amount - 1000) * intr
     else
-      maxSavingsInterest(2000) + (amount - 2000) * 0.1
+      maxSavingsInterest(2000, lw) + (amount - 2000) * intr
   }
 }
 
--- src/test/scala/com/abc/BankTest.scala
+++ src/test/scala/com/abc/BankTest.scala
@@ -1,23 +1,36 @@
 package com.abc
 
 import Account._
-/*
-object main extends App  {
-    val account1 = Savings("John")
-    val bank = Bank("bank1")
-    bank.addAccount(account1,1).deposit(account1, 100, 2).customerReport
- }
-*/
+
 import org.scalatest.{Matchers, FlatSpec}
 
 class BankTest extends FlatSpec with Matchers {
  
   "Bank" should "customer summary" in {
-    
+
     val account1 = Savings("John")
     val bank = Bank("bank1")
-    bank.addAccount(account1,1)
-    .deposit(account1, 100, 2)
-    .customerReport should be("John, 100.0")
+    bank.addAccount(account1,5)
+      .deposit(account1, 100, 11)
+      .deposit(account1, 1000, 15)
+      .deposit(account1, 2000, 18)
+      .deposit(account1, 5000, 20)
+      .customerReport(account1,40) should be("John, 8424.871146928395")
+
+    val account2 = Checking("Jim")
+    bank.addAccount(account2,5)
+      .deposit(account2, 100, 11)
+      .deposit(account2, 1000, 15)
+      .deposit(account2, 2000, 18)
+      .deposit(account2, 5000, 20)
+      .customerReport(account2,40) should be("Jim, 8273.66409947586")
+
+    val account3 = MaxSavings("Jack")
+    bank.addAccount(account3,5)
+      .deposit(account3, 100, 11)
+      .deposit(account3, 1000, 15)
+      .deposit(account3, 2000, 18)
+      .deposit(account3, 5000, 20)
+      .customerReport(account3,40) should be("Jack, 54057.77422840032")
   }
 }
