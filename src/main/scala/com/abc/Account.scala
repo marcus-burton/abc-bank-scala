d63caab3813050a9388610c11563d42ee92b84a6
jambabino <jam.babino@gmail.com>
2016-10-15 15:29:40 -0700

    Improved readability of customer test and enabled all tests

--- src/main/scala/com/abc/Account.scala
+++ src/main/scala/com/abc/Account.scala
@@ -2,13 +2,7 @@ package com.abc
 
 import scala.collection.mutable.ListBuffer
 
-object Account {
-  final val CHECKING: Int = 0
-  final val SAVINGS: Int = 1
-  final val MAXI_SAVINGS: Int = 2
-}
-
-class Account(val accountType: Int, transactions_ : ListBuffer[Transaction] = ListBuffer()) {
+class Account(val accountType: AccountType, transactions_ : ListBuffer[Transaction] = ListBuffer()) {
 
   def transactions: List[Transaction] = transactions_.toList
 
@@ -21,20 +15,7 @@ class Account(val accountType: Int, transactions_ : ListBuffer[Transaction] = Li
     transactions_ += Transaction(amount)
   }
 
-  def interestEarned: Double = {
-    val amount: Double = sumTransactions()
-    accountType match {
-      case Account.SAVINGS =>
-        if (amount <= 1000) amount * 0.001
-        else 1 + (amount - 1000) * 0.002
-      case Account.MAXI_SAVINGS =>
-        if (amount <= 1000) return amount * 0.02
-        if (amount <= 2000) return 20 + (amount - 1000) * 0.05
-        70 + (amount - 2000) * 0.1
-      case _ =>
-        amount * 0.001
-    }
-  }
+  def interestEarned: Double = accountType.interestEarned(amount = sumTransactions())
 
   def sumTransactions(checkAllTransactions: Boolean = true): Double = transactions.map(_.amount).sum
 }
