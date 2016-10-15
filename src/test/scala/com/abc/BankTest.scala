cbc6fe15a19670cef8a4d4037ae3bca86f074753
jambabino <jam.babino@gmail.com>
2016-10-15 16:16:58 -0700

    Nested account class in Customer class

--- src/test/scala/com/abc/BankTest.scala
+++ src/test/scala/com/abc/BankTest.scala
@@ -7,33 +7,53 @@ class BankTest extends FlatSpec with Matchers {
   "Bank" should "customer summary" in {
     val bank: Bank = new Bank
     val john: Customer = new Customer("John")
-      .openAccount(new Account(Checking))
+      .openAccount(Checking)
     bank.addCustomer(john)
     bank.customerSummary should be("Customer Summary\n - John (1 account)")
   }
 
   it should "checking account" in {
     val bank: Bank = new Bank
-    val checkingAccount: Account = new Account(Checking)
-    val bill: Customer = new Customer("Bill").openAccount(checkingAccount)
+    val bill: Customer = new Customer("Bill")
+      .openAccount(Checking)
     bank.addCustomer(bill)
-    checkingAccount.deposit(100.0)
+
+    val checkingAccount = bill.accounts
+      .collectFirst {
+        case account if account.accountType == Checking => account
+      }
+    checkingAccount.foreach(_.deposit(100.0))
+
     bank.totalInterestPaid should be(0.1)
   }
 
   it should "savings account" in {
     val bank: Bank = new Bank
-    val checkingAccount: Account = new Account(Savings)
-    bank.addCustomer(new Customer("Bill").openAccount(checkingAccount))
-    checkingAccount.deposit(1500.0)
+    val bill: Customer = new Customer("Bill")
+      .openAccount(Savings)
+
+    bank.addCustomer(bill)
+
+    val savingAccount = bill.accounts
+      .collectFirst {
+        case account if account.accountType == Savings => account
+      }
+    savingAccount.foreach(_.deposit(1500.0))
+
     bank.totalInterestPaid should be(2.0)
   }
 
   it should "maxi savings account" in {
     val bank: Bank = new Bank
-    val checkingAccount: Account = new Account(MaxiSavings)
-    bank.addCustomer(new Customer("Bill").openAccount(checkingAccount))
-    checkingAccount.deposit(3000.0)
+    val bill: Customer = new Customer("Bill")
+        .openAccount(MaxiSavings)
+    bank.addCustomer(bill)
+
+    val maxiSavingsAccount: Option[bill.Account] = bill.accounts.collectFirst {
+      case account if account.accountType == MaxiSavings => account
+    }
+    maxiSavingsAccount.foreach(_.deposit(3000.0))
+
     bank.totalInterestPaid should be(170.0)
   }
 
--- src/test/scala/com/abc/CustomerTest.scala
+++ src/test/scala/com/abc/CustomerTest.scala
@@ -4,12 +4,18 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class CustomerTest extends FlatSpec with Matchers {
   "Customer statement" should "show transactions and totals for each of their accounts" in {
-    val checkingAccount: Account = new Account(Checking)
-    val savingsAccount: Account = new Account(Savings)
-    val henry: Customer = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount)
-    checkingAccount.deposit(100.0)
-    savingsAccount.deposit(4000.0)
-    savingsAccount.withdraw(200.0)
+
+    val henry: Customer = new Customer("Henry")
+      .openAccount(accountType = Checking)
+      .openAccount(accountType = Savings)
+
+    henry.accounts.foreach {
+      case account if account.accountType == Checking => account.deposit(100.0)
+      case account if account.accountType == Savings =>
+        account.deposit(4000.0)
+        account.withdraw(200.0)
+    }
+
     henry.statement should be("Statement for Henry\n" +
       "\nChecking Account\n  deposit $100.00\nTotal $100.00\n" +
       "\nSavings Account\n  deposit $4000.00\n  withdrawal $200.00\nTotal $3800.00\n" +
@@ -18,23 +24,23 @@ class CustomerTest extends FlatSpec with Matchers {
 
   "Customer" should "be able to open a account" in {
     val oscar: Customer = new Customer("Oscar")
-      .openAccount(new Account(Savings))
+      .openAccount(accountType = Checking)
     oscar.numberOfAccounts should be(1)
   }
 
   it should "be able to open two accounts" in {
     val oscar: Customer = new Customer("Oscar")
-      .openAccount(new Account(Savings))
-      .openAccount(new Account(Checking))
+      .openAccount(accountType = Savings)
+      .openAccount(accountType = Checking)
 
     oscar.numberOfAccounts should be(2)
   }
 
   it should "be able to open three accounts" in {
     val oscar: Customer = new Customer("Oscar")
-      .openAccount(new Account(Savings))
-      .openAccount(new Account(Checking))
-      .openAccount(new Account(MaxiSavings))
+      .openAccount(accountType = Savings)
+      .openAccount(accountType = Checking)
+      .openAccount(accountType = MaxiSavings)
 
     oscar.numberOfAccounts should be(3)
   }
